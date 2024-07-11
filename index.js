4613bcee0c5953992c50664a6b0ecd6e76ced8c6
Codinggenix <109884345+Avi011010@users.noreply.github.com>
2024-07-10 22:52:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -189,3 +189,9 @@ window.addEventListener('popstate', function(event) {
     location.reload();
 });
 
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        window.location.href = this.href;
+    });
+});
